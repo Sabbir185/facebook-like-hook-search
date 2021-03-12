66836cb5b960184998394182e653a43fc41ffb41
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-03-12 20:14:31 +0600

    facebook like action details

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import { useState } from 'react';
 
 function App() {
+  const [thumbLike, setThumbLike] = useState('');
+
+  const handleClick = () =>{
+    const thumbAction = thumbLike ? '' : '#3498DB' ;
+    setThumbLike(thumbAction);
+  }
+
+  const thumbUpStyle = {
+        fontSize:'100px',
+        color:`${thumbLike}`,
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+
+        <ThumbUpIcon style = {thumbUpStyle} onClick={handleClick}></ThumbUpIcon>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
